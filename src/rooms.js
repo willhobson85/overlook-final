82ecfc42adfc1802278d7d9fa66f802fdd0a0aa3
William Hobson <will.hobson85@gmail.com>
2022-08-05 13:00:47 -0400

    Create sample data and Room data and test

---
+++ src/rooms.js
@@ -0,0 +1,12 @@
+class Rooms {
+    constructor(number, type, bidet, bedSize, numBeds, costPerNight) {
+        this.number = number;
+        this.roomType = type;
+        this.bidet = bidet;
+        this.bedSize = bedSize;
+        this.numBeds = numBeds;
+        this.costPerNight = costPerNight
+    }
+}
+
+export default Rooms;
---
+++ src/sample-data.js
@@ -0,0 +1,111 @@
+const userSample = [
+        {
+            "id": 1,
+            "name": "Leatha Ullrich"
+        },
+        {
+            "id": 21,
+            "name": "Kelsie Rath"
+        },
+        {
+            "id": 33,
+            "name": "Rae Wisozk"
+        },
+        {
+            "id": 49,
+            "name": "Arnulfo Simonis"
+        }
+    ]
+
+const roomSample = [
+        {
+            "number": 1,
+            "roomType": "residential suite",
+            "bidet": true,
+            "bedSize": "queen",
+            "numBeds": 1,
+            "costPerNight": 358.4
+        },
+        {
+            "number": 8,
+            "roomType": "junior suite",
+            "bidet": false,
+            "bedSize": "king",
+            "numBeds": 1,
+            "costPerNight": 261.26
+        },
+        {
+            "number": 12,
+            "roomType": "single room",
+            "bidet": false,
+            "bedSize": "twin",
+            "numBeds": 2,
+            "costPerNight": 172.09
+        },
+        {
+            "number": 22,
+            "roomType": "single room",
+            "bidet": false,
+            "bedSize": "full",
+            "numBeds": 2,
+            "costPerNight": 350.31
+        }
+    ]
+
+const bookingSample = [
+        {
+            "id": "5fwrgu4i7k55hl6t8",
+            "userID": 1,
+            "date": "2022/02/05",
+            "roomNumber": 12,
+
+        },
+        {
+            "id": "5fwrgu4i7k55hl727",
+            "userID": 1,
+            "date": "2022/11/06",
+            "roomNumber": 22,
+        
+        },
+        {
+            "id": "5fwrgu4i7k55hl72m",
+            "userID": 21,
+            "date": "2022/01/09",
+            "roomNumber": 13,
+        
+        },
+        {
+            "id": "5fwrgu4i7k55hl73z",
+            "userID": 21,
+            "date": "2022/01/31",
+            "roomNumber": 10,
+        
+        },
+        {
+            "id": "5fwrgu4i7k55hl747",
+            "userID": 33,
+            "date": "2023/11/31",
+            "roomNumber": 13,
+        
+        },
+        {
+            "id": "5fwrgu4i7k55hl765",
+            "userID": 33,
+            "date": "2023/12/23",
+            "roomNumber": 10,
+        },
+        {
+            "id": "5fwrgu4i7k55hl76d",
+            "userID": 49,
+            "date": "2022/01/08",
+            "roomNumber": 19,
+        },
+        {
+            "id": "5fwrgu4i7k55hl76w",
+            "userID": 49,
+            "date": "2022/02/24",
+            "roomNumber": 2,
+        }
+    ]
+
+export {userSample, roomSample, bookingSample}
