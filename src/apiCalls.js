a4a838bec80e61f73f8b9d4b77d3505e9c36d5ac
William Hobson <will.hobson85@gmail.com>
2022-08-07 13:31:26 -0400

    Correct api links

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,13 +1,13 @@
-const customersAPIData = fetch("http://localhost:3001/api/v1/users")
+const customersAPIData = fetch("http://localhost:3001/api/v1/customers")
   .then((response) => response.json())
   .catch((error) => console.log(error));
 
-const roomsAPIData = fetch("http://localhost:3001/api/v1/ingredients")
+const roomsAPIData = fetch("http://localhost:3001/api/v1/rooms")
   .then((response) => response.json())
   .catch((error) => console.log(error));
 
-const bookingsAPIData = fetch("http://localhost:3001/api/v1/recipes")
+const bookingsAPIData = fetch("http://localhost:3001/api/v1/bookings")
   .then((response) => response.json())
   .catch((error) => console.log(error));
 
-export { customersAPIData, roomssAPIData, bookingsAPIData };
+export { customersAPIData, roomsAPIData, bookingsAPIData };
